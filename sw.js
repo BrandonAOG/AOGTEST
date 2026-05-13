7960d6849ca534ffa13ea89b92ae8022c2c668a8
BrandonAOG <brandon@alwaysongenerators.com>
2026-05-13 15:34:09 -0400

    Bump CACHE_NAME to v1.0.7
    Updated CACHE_NAME to version 1.0.7 and modified CHANGELOG.

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v1.0.6';
+var CACHE_NAME = 'aog-forms-v1.0.7';
 var DEV_MODE   = false; // ← SET TRUE during development/testing
 
 // ============================================================
@@ -20,7 +20,7 @@ var CHANGELOG = [
   'fixed transfer switch selectiions',
   'fixed image capture bug on estimate form',
   'add more 1st stage and 2nd stage regulators for more variance',
-  'ADDED 24KW & 26KW Generac Gaurdian legacy Generators'
+  'testing est 1'
 ];
 // ============================================================
 
