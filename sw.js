929e7320def476fb2644bec92af158891ab93d9b
BrandonAOG <brandon@alwaysongenerators.com>
2026-05-13 17:11:37 -0400

    Update CACHE_NAME to version 1.0.14
    Bumped CACHE_NAME to version 1.0.14.

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v1.0.13';
+var CACHE_NAME = 'aog-forms-v1.0.14';
 var DEV_MODE   = false; // ← SET TRUE during development/testing
 
 // ============================================================
