4058088ecd5edeef607cf6351e273cc63b4c74c1
BrandonAOG <brandon@alwaysongenerators.com>
2026-05-14 05:08:43 -0400

    Bump CACHE_NAME to version 1.0.38

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v1.0.37';
+var CACHE_NAME = 'aog-forms-v1.0.38';
 var DEV_MODE   = false; // ← SET TRUE during development/testing
 
 // ============================================================
@@ -17,7 +17,7 @@ var DEV_MODE   = false; // ← SET TRUE during development/testing
 //  Keep each line short — one change per item.
 // ============================================================
 var CHANGELOG = [
-  'testing estimate sheet 17 ',
+  'testing estimate sheet 18 ',
   'fixed image capture bug on estimate form',
   'add more 1st stage and 2nd stage regulators for more variance',
   'testing est 4'
