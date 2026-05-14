3a5ab56426d19ff086f3dcbe8bc43ee516c21ea5
BrandonAOG <brandon@alwaysongenerators.com>
2026-05-13 22:15:50 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v1.0.26';
+var CACHE_NAME = 'aog-forms-v1.0.27';
 var DEV_MODE   = false; // ← SET TRUE during development/testing
 
 // ============================================================
@@ -17,7 +17,7 @@ var DEV_MODE   = false; // ← SET TRUE during development/testing
 //  Keep each line short — one change per item.
 // ============================================================
 var CHANGELOG = [
-  'testing estimate sheet 6',
+  'testing estimate sheet 7 ',
   'fixed image capture bug on estimate form',
   'add more 1st stage and 2nd stage regulators for more variance',
   'testing est 4'
