ec3c11cecd5f5f74b249eb0e5de087d7d2cd5577
BrandonAOG <brandon@alwaysongenerators.com>
2026-05-23 09:52:05 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v1.0.65';
+var CACHE_NAME = 'aog-forms-v1.0.66';
 var DEV_MODE   = false; // ← SET TRUE during development/testing
 
 // ============================================================
