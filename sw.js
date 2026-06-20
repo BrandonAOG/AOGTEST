bcd798a34708a30c3c852a16f91066248158c110
BrandonAOG <brandon@alwaysongenerators.com>
2026-06-20 10:38:34 -0400

    Update CACHE_NAME to version 2.2.2TEST

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //    2. Update CHANGELOG below with what changed
 // ============================================================
 
-var CACHE_NAME = 'aog-forms-v2.2.1TEST';
+var CACHE_NAME = 'aog-forms-v2.2.2TEST';
 var DEV_MODE   = false;
 
 // Stores last known cache progress so late-loading pages can request it
